d5dd995f5d387e29acbfafd59179af6d9b94cf7c
vansonha <eusvaha@gmail.com>
2021-05-25 07:26:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppjenkinsGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
