91763083691b5bf865ec4ce414c2bcb9e5ccf9c6
vansonha <eusvaha@gmail.com>
2021-05-25 07:28:01 -0700

    1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppjenkinsGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Build 1");
         }
     }
 }
